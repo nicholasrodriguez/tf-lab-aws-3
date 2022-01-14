e9b178fa6f524ae1da3913b8a6eb5f692fa3ac73
nicholasrodriguez <nicholasrodriguez@users.noreply.github.com>
2022-01-14 17:21:03 +0000

    Added instance config

--- main.tf
+++ main.tf
@@ -24,11 +24,27 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "example_a" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.ec2_instance_type
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    EOF
 }
 
 resource "aws_instance" "example_b" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.ec2_instance_type
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    EOF
 }
 
 resource "aws_eip" "ip" {
